428b9705efdaed26d74f6e55887ea87b2202fa07
Ash <ashley@juice49.com>
2023-09-15 22:09:07 +0100

    Use groqfmt from npm

--- groqfmt.d.ts
+++ groqfmt.d.ts
@@ -1,24 +1,2 @@
-interface GroqfmtError {
-  begin: number
-  end?: number
-  message: string
-}
-
-type GroqfmtResult =
-  | {
-      error: GroqfmtError
-      result: undefined
-    }
-  | {
-      error: undefined
-      result: string
-    }
-
-declare const groqfmt: (query: string) => GroqfmtResult
-
-declare const Go: {
-  new (): {
-    run: (instance: WebAssembly.Instance) => Promise<void>
-    importObject: WebAssembly.Imports
-  }
-}
+declare const groqfmt: import('@groqfmt/wasm').Groqfmt
+declare const Go: import('@groqfmt/wasm').Go
--- lib/load-groqfmt.ts
+++ lib/load-groqfmt.ts
@@ -1,7 +1,7 @@
+import { type Groqfmt } from '@groqfmt/wasm'
+
 // TODO: This can probably be a generic `loadGoLang` function.
-export default async function loadGroqfmt(): Promise<
-  ((query: string) => GroqfmtResult) | undefined
-> {
+export default async function loadGroqfmt(): Promise<Groqfmt | undefined> {
   if (typeof window === 'undefined') {
     return
   }
@@ -9,9 +9,7 @@ export default async function loadGroqfmt(): Promise<
   const go = new Go()
 
   const { instance } = await WebAssembly.instantiateStreaming(
-    fetch(
-      '/groqfmt-full-a114f73af5a94907e425ef0e342e2c51e8a39a0801b6766394cc912017cc1845.wasm',
-    ),
+    fetch('/groqfmt.wasm'),
     go.importObject,
   )
 
